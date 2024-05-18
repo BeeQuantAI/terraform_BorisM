8573efc8bfd73f010a53376d03302bbb1d2ce87c
captainboris <captainboris246281@gmail.com>
2024-05-18 16:22:52 +1000

    added remote state

--- securitygroup.tf
+++ securitygroup.tf
@@ -54,13 +54,13 @@ resource "aws_security_group" "grafana_sg" {
     Name = "allow_grafana_tcp"
   }
 }
-resource "aws_vpc_security_group_ingress_rule" "db_sg_inbound" {
-  security_group_id = aws_security_group.db_sg.id
-  ip_protocol       = "tcp"
+resource "aws_vpc_security_group_ingress_rule" "grafana_sg_inbound" {
+  security_group_id = aws_security_group.grafana_sg.id
+  ip_protocol       = "-1"
   cidr_ipv4         = "0.0.0.0/0"
 }
-resource "aws_vpc_security_group_egress_rule" "db_sg_outbound" {
-  security_group_id = aws_security_group.db_sg.id
+resource "aws_vpc_security_group_egress_rule" "grafana_sg_outbound" {
+  security_group_id = aws_security_group.grafana_sg.id
   # from_port         = 0
   ip_protocol       = "-1"
   # to_port           = 0
